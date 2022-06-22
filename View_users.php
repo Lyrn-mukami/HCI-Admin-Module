5984e87c78720f51a7196bbb1287da4666f3b8f9
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2022-06-22 12:45:39 +0300

    Update View_users.php

--- View_users.php
+++ View_users.php
@@ -22,7 +22,7 @@
 			  <div class="profile">
 			  <img src="Profile_images/avatar_1.png">
 			  <div class="dropdown-content">
-			  	<a href="Profile.html">Profile</a>
+			  	<a href="Profile.php">Profile</a>
 			  	<a href="logout.php">Log out</a>
 			  </div>
 			  </div>
